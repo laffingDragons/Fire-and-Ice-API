f55ee48ec5ee5a791d9f56d8598d57c8cad17e93
laffingragons <akshaypatil,vit@gmail.com>
2017-12-29 22:17:08 +0530

    bookModel

--- app.js
+++ app.js
@@ -18,7 +18,111 @@ mongoose.connection.once('open', function () {
 });
 //include the model file
 
-var Blog = require('./bookModel.js');
+var Book = require('./bookModel.js');
 
-var blogModel = mongoose.model('Book');
+var bookModel = mongoose.model('Book');
 //ends include
+
+//here are the routes
+app.get('/', function (req, res) {
+    res.send('This is a GOT Application')
+});
+
+////// lets write code here for the routes//////
+
+//start route to GEt allbooks
+app.get('/books', function (req, res) {
+
+    blogModel.find(function (err, result) {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(result)
+        }
+    }); // end user model find
+});
+
+//to get single blog
+app.get('/books/:id', function (req, res) {
+    blogModel.findOne({
+        '_id': req.params.id
+    }, function (err, result) {
+        if (err) {
+            console.log("some Error");
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    }); //end user model find
+}); // end route to get a particular blog
+
+//to create a blog
+app.post('/book/create', function (req, res) {
+    var newBlog = new blogModel({
+        title: req.body.title,
+        subTitle: req.body.subTitle,
+        blogBody: req.body.blogBody
+    }); //end newBlog
+
+    //lets set the date of creation
+    var today = Date.now();
+    newBlog.created = today;
+
+    //lets set the tags into array
+    var allTags = (req.body.allTags != undefined && req.body.allTags != null) ? req.body.allTags.split(',') : '';
+    newBlog.tags = allTags;
+
+    //lets set the author information
+    var authorInfo = {
+        fullName: req.body.authorFullName,
+        email: req.body.authorEmail
+    };
+    newBlog.authorInfo = authorInfo;
+
+    // now lets save the file
+    newBlog.save(function (error) {
+        if (error) {
+            console.log(error);
+            res.send(error);
+        } else {
+            res.send(error);
+        }
+    }); //end new blog save
+
+}); // 
+
+//start route to edit a blog using _id
+
+app.put('/books/:id/edit', function(req, res) {
+
+    var update = req.body;
+
+    blogModel.findOneAndUpdate({'_id': req.params.id}, update, function (err, result) {
+
+        if (err) {
+            console.log("some Error");
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    }); //end user model find
+});
+
+//start the route to delete a blog
+app.post('/books/:id/delete',function(req, res){
+    
+    blogModel.remove({'_id':req.params.id},function(err, result){
+        if(err){
+           console.log("some Error");
+           res.send(err);
+       }
+       else{
+           res.send(result);
+       }
+    });//end user model find
+});
+
+//
+app.listen(3000, function(){
+    console.log('Listening on Port 3000');
+});
